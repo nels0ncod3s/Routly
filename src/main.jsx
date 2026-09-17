2ca760cb4baffdf7af831c7cc861fcd0a7d4367b
Nelson Wey <oluwademiladewey@gmail.com>
2026-09-17 15:11:36 +0100

    Load location search styles

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import 'leaflet/dist/leaflet.css'
 import './index.css'
 import './hero.css'
+import './location-search.css'
 import App from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
