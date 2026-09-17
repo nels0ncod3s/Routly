1153eb5fefc5282b7ed50e0b6f1f07aed92ce910
Nelson Wey <oluwademiladewey@gmail.com>
2026-09-17 14:50:24 +0100

    Switch to redesigned hero

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import 'leaflet/dist/leaflet.css'
 import './index.css'
-import './hero-v2.css'
+import './hero-v3.css'
 import App from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
